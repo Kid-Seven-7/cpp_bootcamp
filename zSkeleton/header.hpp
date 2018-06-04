059f1b01ab07fea310b204396588861438a44918
Kid-Seven-7 <joseph.ngoma@moov.life>
2018-06-04 13:39:42 +0200

    Added skeleton header

--- zSkeleton/header.hpp
+++ zSkeleton/header.hpp
@@ -1,4 +1,13 @@
 #ifndef MYHEADER
 #define MYHEADER
     ......
+
+		class class_name {
+		  access_specifier_1:
+		    member1;
+		  access_specifier_2:
+		    member2;
+		  ...
+		} object_names;
+
 #endif
