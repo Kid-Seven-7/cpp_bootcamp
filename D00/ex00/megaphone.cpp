65b0d94ca6c9ebc18c19836193de4ea27194db20
Kid-Seven-7 <joseph.ngoma@moov.life>
2018-06-04 07:48:49 +0200

    ex00 done

---
+++ D00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <cctype>
+
+int main(int ac, char **av){
+	std::string sMessage;
+
+	if (ac == 1)
+		std::cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE*";
+	else{
+		for (int i = 1; i < ac; ++i){
+			for (int j = 0; av[i][j] != '\0'; ++j){
+				if ((!isupper(av[i][j])) && isalpha(av[i][j]))
+					av[i][j] -= 32;
+			}
+			std::cout << av[i];
+		}
+	}
+	std::cout << std::endl;
+}
