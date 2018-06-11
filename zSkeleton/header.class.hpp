f9318a8c71dc5ce51d207c15429225963e3bf93b
Kid-Seven-7 <joseph.ngoma@moov.life>
2018-06-11 09:52:16 +0200

    Moved implementations into x.class.cpp file and some other minor changes

--- zSkeleton/header.class.hpp
+++ zSkeleton/header.class.hpp
@@ -10,14 +10,12 @@ class class_name {
 			float;
 			bool;
 		*/
-		//for copy constuctor
-		class_name *ptr;
 
 	protected:
 
 	public:
 		//static variables
-			int NumOf;
+			static int NumOf;
 
 		/* Return types
 			std::string;
@@ -28,94 +26,44 @@ class class_name {
 		*/
 
 		//getters
-			[some_return_type] Get();
-			/* Implementation
-				std::string Get(){return (this-> [some_variable]);}
-			*/
+			some_return_type Get();
 
 		//setters
-			void Set();
-			/* Implementation
-				void Set(){this->[some_variable] = [some_variable];}
-			*/
+			void Set(data_type variable_name);
 
 		//constructor(s)
 			//default
 			class_name();
-			/* Implementation
-				class_name::class_name(){
-					std::cout << "Default constuctor called" << std::endl;
-					this->[some_variable] = [some_other_variable]
-					class_name::NumOf++;
-				}
-			*/
 
 			//copy
-			class_name(const class_name &[some_variable_name]);
-			/* Implementation
-				class_name::class_name(const class_name [some_variable_name]){
-					std::cout << "Copy constuctor called" << std::endl;
-					ptr = new class_name;
-					*ptr = *[some_variable_name].ptr
-					this->[some_variable] = [some_other_variable]
-					class_name::NumOf++;
-				}
-			*/
+			class_name(const class_name & some_variable_name);
 
 			//simple
-			class_name([some_variable]...);
-			/* Implementation
-				class_name::class_name([some_variable]...){
-					std::cout << "A simple constuctor called" << std::endl;
-					this->[some_variable] = [some_other_variable]
-					class_name::NumOf++;
-				}
-			*/
+			class_name(some_variable);
+
 		//destructor
 			~class_name();
-			/* Implementation
-				class_name::~class_name(){
-					std::cout << "Destructor called\n";
-				}
-			*/
 
 		//Member functions
-			void SetAll([some_variable]...);
-			/* Implementation
-				void class_name::SetAll([some_variable]...){
-					this->[some_variable] = [some_other_variable]
-					class_name::NumOf++;
-				}
-			*/
+			void SetAll(some_variable);
 
 		//Overloads
-			//Assign Overload
+			//Operator Overloads
 			class_name & operator=(class_name const & rhs);
-			/* Implementation
-				class_name & class_name::operator=(const class_name [some_variable1] & rhs){
-					std::cout
-					<< "Assignment operator called"
-					<< '\n';
-
-					this->[some_variable] = rhs.[some_member_function]();
-
-					return (*this);
-				}
-			*/
+			class_name & operator==(class_name const & rhs);
+			class_name & operator!=(class_name const & rhs);
+			class_name & operator<(class_name const & rhs);
+			class_name & operator>(class_name const & rhs);
+			class_name & operator<=(class_name const & rhs);
+			class_name & operator>=(class_name const & rhs);
+			class_name & operator<<(class_name const & rhs);
+			class_name & operator>>(class_name const & rhs);
 
 			//ToString Overload
 			void ToString(); //Handy override for displaying info
-			/* Implementation
-				void class_name::ToString(){
-					std::cout << "some_message : " << some_variable << std::endl;
-				}
-			*/
 
 		//Static functions
-			int GetNumOf();
-			/* Implementation
-				int GetNumOf(){return (NumOf);}
-			*/
+			static int GetNumOf();
 
 };
 
